e8356a0e21c99da943b6f237aa53527aafc7f778
dhanushkumar5 <r.danush2001@gmail.com>
2024-03-21 14:11:17 +0530

    url changes 2

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/world-wise",
+  base: "/world-wise/",
   plugins: [react(), eslint()],
 });
